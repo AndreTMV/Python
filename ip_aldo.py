fd1e87687881a3059ae7c53d92e89857b2994bea
Andre Mejia <andretmv12345@gmail.com>
2021-11-23 18:48:40 -0600

    Another stupid commit

---
+++ ip_aldo.py
@@ -0,0 +1,8 @@
+# import ipaddress as ip
+# ip_address = ip.IPv4Network('192.34.198.0/25')
+# for addrs in ip_address:
+#     print(f"{addrs},")
+# for number in range(0,25):
+#     print(f"Fa0/{number}")
+for number in range(20):
+    print("255.255.255.0")
---
+++ mac.py
@@ -0,0 +1,7 @@
+import random
+for number in range(20):
+    mac = [ 0x00, 0x16, 0x3e,
+           random.randint(0x00, 0x7f),
+           random.randint(0x00, 0xff),
+           random.randint(0x00, 0xff) ]
+    print (':'.join(map(lambda x: "%02x" % x, mac)))
